4f1e7e03339a6d0a05ffa2e73d39a99fd4a6d801
alan <swd-004@mail.foxconn.com>
2018-01-02 16:16:59 +0800

    add wood

---
+++ app/src/main/java/com/example/alan/magictower/obstacle/factory/IObstacleWoodFactory.java
@@ -0,0 +1,29 @@
+package com.example.alan.magictower.obstacle.factory;
+
+import com.example.alan.magictower.obstacle.wood.ObstacleWood;
+
+/**
+ * Function :
+ * Modify Date : 2018/1/2
+ *
+ * @Author : Alan
+ * Issue : TODO
+ * Whether Solve :
+ */
+
+public class IObstacleWoodFactory implements IObstacleFactory {
+
+    private ObstacleWood wood;
+
+    public IObstacleWoodFactory(int x, int y, boolean exist) {
+        wood = new ObstacleWood(x, y, exist);
+    }
+
+    @Override
+    public ObstacleWood createObstacle() {
+        if (wood == null){
+            return null;
+        }
+        return wood;
+    }
+}
---
+++ app/src/main/java/com/example/alan/magictower/obstacle/wood/ObstacleWood.java
@@ -0,0 +1,20 @@
+package com.example.alan.magictower.obstacle.wood;
+
+import com.example.alan.magictower.obstacle.Obstacle;
+
+/**
+ * Function :
+ * Modify Date : 2018/1/2
+ *
+ * @Author : Alan
+ * Issue : TODO
+ * Whether Solve :
+ */
+
+public class ObstacleWood extends Obstacle {
+
+    public ObstacleWood(int x, int y, boolean exist) {
+        super(x, y, exist);
+    }
+
+}
